0d331697ac24d2cb96c460550960ab041e6898a0
mathf07 <mathf07@laposte.net>
2015-07-20 15:50:40 +0200

    all

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,13 +11,13 @@ class MoviesController < ApplicationController
 	end
 	def watch
 		@movie = Movie.find(params[:id])
-		line = Cocaine::CommandLine.new("vlc --fullscreen '/home/sophie/projets/videos/#{@movie.url}'")
+		line = Cocaine::CommandLine.new("vlc --fullscreen '/home/math/videos_2015/#{@movie.url}'")
 		line.run
 		render 'show'
 	end
 	def download
 		@movie = Movie.find(params[:id])
-		send_file("/../../../../home/sophie/projets/videos/#{@movie.url}")
+		send_file("/../../../../home/math/videos_2015/#{@movie.url}")
 	end
 	
 	private
