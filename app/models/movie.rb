0b3fefd000c4fe85fbcef8737fa5c70c79bab188
parmeso6 <sophie.marie.parmentier@gmail.com>
2015-07-27 14:45:17 +0200

    ?

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,8 +1,5 @@
-require 'elasticsearch/model'
+
 
 class Movie < ActiveRecord::Base
-  include Elasticsearch::Model
-  include Elasticsearch::Model::Callbacks
+
 end
-Movie.create!
-Movie.import # for auto sync model with elastic search
